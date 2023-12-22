3033cca9774a45845c74f2d8458b24e8bd619584
startg2545 <74952330+startg2545@users.noreply.github.com>
2023-12-22 17:26:29 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    return {
+        restrict: 'E',
+        templateUrl: 'app/components/member/member.html',
+        controller: 'MemberController',
+        controllerAs: 'vm',
+        bindToController: true,
+        scope: {
+            member: '='
+        }
+    };
+}
